c7e8093bfe8cc832fbeb5129ee6ffdef3f8b2890
Piepow <piepow@users.noreply.github.com>
2020-06-08 22:35:59 -0400

    Don't use prediction when spectating

--- src/game/client/gameclient.cpp
+++ src/game/client/gameclient.cpp
@@ -1640,7 +1640,8 @@ bool CGameClient::ShouldUsePredicted() {
 				GAMESTATEFLAG_ROUNDOVER |
 				GAMESTATEFLAG_GAMEOVER
 			)
-		);
+		) &&
+		!(m_Snap.m_SpecInfo.m_Active);
 }
 
 bool CGameClient::ShouldUsePredictedLocalChar() {
