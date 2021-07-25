1de9d412cbf9ce2b05fbff4d613c5d9ea1f0a1f1
Robert Müller <robert.mueller@uni-siegen.de>
2021-07-25 19:38:42 +0200

    remove obsolete mem_alloc alignment parameter

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -2532,7 +2532,7 @@ void CClient::RegisterCommands()
 
 static CClient *CreateClient()
 {
-	CClient *pClient = static_cast<CClient *>(mem_alloc(sizeof(CClient), 1));
+	CClient *pClient = static_cast<CClient *>(mem_alloc(sizeof(CClient)));
 	mem_zero(pClient, sizeof(CClient));
 	return new(pClient) CClient;
 }
--- src/game/editor/io.cpp
+++ src/game/editor/io.cpp
@@ -231,7 +231,7 @@ int CEditorMap::Save(class IStorage *pStorage, const char *pFileName)
 
 	// save points
 	int TotalSize = Size * PointCount;
-	unsigned char *pPoints = (unsigned char *)mem_alloc(TotalSize, 1);
+	unsigned char *pPoints = (unsigned char *)mem_alloc(TotalSize);
 	int Offset = 0;
 	for(int e = 0; e < m_lEnvelopes.size(); e++)
 	{
@@ -341,9 +341,10 @@ int CEditorMap::Load(class IStorage *pStorage, const char *pFileName, int Storag
 					pImg->m_Format = CImageInfo::FORMAT_RGBA;
 
 					// copy image data
+					int DataSize = pImg->m_Width * pImg->m_Height * PixelSize;
 					void *pData = DataFile.GetData(pItem->m_ImageData);
-					pImg->m_pData = mem_alloc(pImg->m_Width*pImg->m_Height*4, 1);
-					mem_copy(pImg->m_pData, pData, pImg->m_Width*pImg->m_Height*4);
+					pImg->m_pData = mem_alloc(DataSize);
+					mem_copy(pImg->m_pData, pData, DataSize);
 					pImg->m_Texture = m_pEditor->Graphics()->LoadTextureRaw(pImg->m_Width, pImg->m_Height, pImg->m_Format, pImg->m_pData, CImageInfo::FORMAT_AUTO, IGraphics::TEXLOAD_MULTI_DIMENSION);
 				}
 
