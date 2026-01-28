teeworlds-community/teeworlds

Fix m_pa* variable naming to m_ap*
Fixed using the following shell script

```
#!/bin/sh

grep -rEo '\bm_paa[A-Z][a-zA-Z0-9]+' src | cut -d':' -f2- | sort -u |
while read -r var
        do
                new="m_aap$(printf '%s' "$var" | cut -c 6-)"
                perl -i -p -e's/\b'"$var"'\b/'"$new"'/g' $(find . -name
'*.h' -o -name '*.cpp' -o -name '*.c')
        done

grep -rEo '\bm_pa[A-Z][a-zA-Z0-9]+' src | cut -d':' -f2- | sort -u |
while read -r var
        do
                new="m_ap$(printf '%s' "$var" | cut -c 5-)"
                perl -i -p -e's/\b'"$var"'\b/'"$new"'/g' $(find . -name
'*.h' -o -name '*.cpp' -o -name '*.c')
        done
```

src/engine/client/textrender.h
@@ -162,7 +162,7 @@ class CTextRender : public IEngineTextRender
 	// support regional variant fonts
 	int m_NumVariants;
 	int m_CurrentVariant;
-	CFontLanguageVariant *m_paVariants;
+	CFontLanguageVariant *m_apVariants;
 
 	FT_Library m_FTLibrary;
 

M0REKZ/mkRace

src/engine/client/textrender.h
@@ -162,7 +162,7 @@ class CTextRender : public IEngineTextRender
 	// support regional variant fonts
 	int m_NumVariants;
 	int m_CurrentVariant;
-	CFontLanguageVariant *m_paVariants;
+	CFontLanguageVariant *m_pVariants;
 
 	FT_Library m_FTLibrary;
 